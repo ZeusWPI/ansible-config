b30e9c45a174787deed46cfe19768dd151cccdd1
redfast00 <redfast00@gmail.com>
2019-09-18 23:15:20 +0200

    Add strobot

---
+++ roles/strobot_zijn_haar/templates/strobot_zijn_haar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# read mail from stdin
+content=$(tee)
+# send original mail
+echo "$content" | procmail "$@"
+
+# base64 encode content
+encoded_content=$(echo "$content" | base64 --wrap=0)
+# send mail to endpoint
+curl -d "$encoded_content" -X POST "{{ endpoint }}"
